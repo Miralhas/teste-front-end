212134c26f1c1adeaa994c13dcd215a7a10cd313
Miralhas <victor2.miralhas@gmail.com>
2025-03-15 00:22:21 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+  return (
+    <main className='container'>
+      <header>
+        <section className="header-top">
+          <div className="information-group">
+            <p>Compra <span className="text-highlight">100% segura</span></p>
+          </div>
+        </section>
+      </header>
+    </main>
+  )
+}
+
+export default App;
