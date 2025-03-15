f20408d369a56df4cd23fcca006f2d9115c4912b
Miralhas <victor2.miralhas@gmail.com>
2025-03-15 00:26:15 +0000

    feat: add scss folder structure and files

---
+++ src/components/header/information-group.tsx
@@ -0,0 +1,14 @@
+type InformationGroup = {
+  icon: any,
+  text: string
+}
+
+const InformationGroup = ({ icon, text }: InformationGroup) => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default InformationGroup;
